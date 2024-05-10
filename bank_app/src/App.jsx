90ebccf23638d505f1275bc83ea66d7fc56712ff
zendayat <nmapzendaya@gmail.com>
2024-05-11 03:37:20 +0400

    update: navbar complete

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -15,28 +15,28 @@ const App = () => (
       {/* NAVBAR */}
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar/>
         </div>
       </div>
       
       {/* HERO */}
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero/>
         </div>
       </div>
 
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          CTA
-          Footer
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
         </div>
       </div>
     </div>
