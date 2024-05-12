f03aa5c6011a3b3ed606bf2d0381b639c161fb10
zendayat <nmapzendaya@gmail.com>
2024-05-12 21:18:59 +0400

    update: completed carddeal component/section

--- bank_app/src/components/Button.jsx
+++ bank_app/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({styles}) => {
   return (
-    <button type="button" className={`${styles} py-4 px-6 font-poppins font-medium text-[18px] text-primary outline-none bg-blue-gradient`}>
+    <button type="button" className={`${styles} py-4 px-6 font-poppins font-medium text-[18px] text-primary outline-none bg-blue-gradient rounded-[10px]`}>
       Get Started
     </button>
   )
